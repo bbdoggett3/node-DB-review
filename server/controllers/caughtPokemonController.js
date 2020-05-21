4fb6aaee181259d8b0b67bfdae38b9f0398e4947
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-21 12:50:05 -0600

    backened setup

---
+++ server/controllers/caughtPokemonController.js
@@ -0,0 +1,44 @@
+const pokemon = [{id: 0, name: "Pokemon 1", image: "image"}]
+let id = 1
+
+module.exports = {
+    getCaughtPokemon: (req, res) => {
+        res.status(200).send(pokemon)
+    },
+    catchPokemon: (req, res) => {
+        const {name, image} = req.body
+
+        const newPokemon = {id, name, image}
+
+        pokemon.push(newPokemon)
+
+        id++
+
+        res.status(200).send(pokemon)
+    },
+    editPokemonName: (req, res) => {
+        const {pokemon_id} = req.params
+        const {newName} = req.body
+
+        const index = pokemon.findIndex(element => element.id === +pokemon_id)
+
+        if(index === -1) {
+            return res.status(404).send("Pokemon not found")
+        }
+
+        pokemon[index].name = newName
+        res.status(200).send(pokemon)
+    },
+    deletePokemon: (req, res) => {
+        const {pokemon_id} = req.params
+
+        const index = pokemon.findIndex(element => element.id === +pokemon_id)
+
+        if(index === -1) {
+            return res.status(404).send("Pokemon not found")
+        }
+
+        pokemon.splice(index, 1)
+        res.status(200).send(pokemon)
+    }
+}
--- src/server/controllers/wildPokemonController.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    getThreeRandomPokemon: (req, res) => {}
-}
