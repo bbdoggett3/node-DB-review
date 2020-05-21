4bba745024e081afcf618c417fc3cd944a21f9f2
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-21 10:54:51 -0600

    database enpoints setup

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,16 @@ const wildCtrl = require('./controllers/wildPokemonController')
 const caughtCtrl = require('./controllers/caughtPokemonController')
 const SERVER_PORT = 4232
 
-app.use(expres.json())
+app.use(express.json())
 
+//Wild Pokemon endpoints 
+app.get('/api/wild-pokemon', wildCtrl.getThreeRandomPokemon)
+
+//Caught pokemon endpoint
+app.get('/api/pokemon', caughtCtrl.getCaughtPokemon)
+app.post('/api/pokemon', caughtCtrl.catchPokemon)
+app.put('api/pokemon/:pokemon_id', caughtCtrl.editPokemonName)
+app.delete('/api/pokemon/:pokemon_id', caughtCtrl.deletePokemon)
+
+//listening port
 app.listen(SERVER_PORT, () => console.log(`catching em all on port ${SERVER_PORT}`))
